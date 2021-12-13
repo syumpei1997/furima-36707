2a445be235f5d1c5f809f75890273f63a41193b9
syumpei1997 <syumpei1997@gmail.com>
2021-12-13 23:20:39 +0900

    deviseの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-         validates :name, presence: true
-         validates :last_name, presence: true
-         validates :nickname, presence: true
-         validates :email, presence: true
-         validates :encrypted_password, presence: true
-         validates :furigana_name, presence: true
-         validates :furigana_last_name, presence: true
-         validates :birthday, presence: true
-
 end
--- db/migrate/20211212101236_devise_create_users.rb
+++ db/migrate/20211212101236_devise_create_users.rb
@@ -6,6 +6,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,           null: false
+      t.string :furigana_name,      null: false
+      t.string :furigana_last_name, null: false
+      t.string :name,               null: false
+      t.string :last_name,          null: false
+      t.date   :birthday,           null: false
+
 
       ## Recoverable
       t.string   :reset_password_token
