9a8801611915158c6373ef8201777d716627f131
syumpei1997 <syumpei1997@gmail.com>
2021-12-17 21:13:41 +0900

    正規表現の解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :name, presence: true, format: { with: 正規表現 , message: '全角日本語'}
+         validates :name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角日本語'}
          validates :nickname, presence: true, length: { maximum: 6 }
-         validates :furigana_name, presence: true, format: { with: 正規表現 , message: '全角カタカナ'}
-         validates :furigana_last_name, presence: true, format: { with: 正規表現 , message: '全角カタカナ'}
-         validates :last_name, presence: true, format: { with: 正規表現 , message: '全角日本語'}
+         validates :furigana_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角カタカナ'}
+         validates :furigana_last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角カタカナ'}
+         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角日本語'}
          validates :birthday, presence: true
-         validates :encrypted_password, presence: true、format: { with: 正規表現 , message: 'エラーメッセージ'}
+         validates :password, presence: true、format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'エラーメッセージ'}
