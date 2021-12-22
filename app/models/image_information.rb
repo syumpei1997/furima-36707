c565476e90e647752cca27c6838820d7a8a30ab4
syumpei1997 <syumpei1997@gmail.com>
2021-12-22 20:51:28 +0900

    Active hashの導入

--- app/models/image_information.rb
+++ app/models/image_information.rb
@@ -2,4 +2,7 @@ class ImageInformation < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_one    :purchase
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :delivery, :nissuu, :prefecture, :status
 end
