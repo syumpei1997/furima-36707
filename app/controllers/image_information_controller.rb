8889fb51e4633fa40a342de39323f2dfbad833db
syumpei1997 <syumpei1997@gmail.com>
2021-12-23 21:26:16 +0900

    商品出品ページの表示

--- app/controllers/image_information_controller.rb
+++ app/controllers/image_information_controller.rb
@@ -1,4 +1,19 @@
 class ImageInformationController < ApplicationController
+  
+  def new
+    @image_information = Image_information.new
+    redirect_to root_path unless current_user.id == @image_informations.user_id
+  end
+  
+  def create
+    @image_information = Image_information.new(image_infomation_params)
+    if @image_information.save
+        render 'index'
+    else
+        render 'new'
+    end
+  end
+
   private
 
   def message_params
