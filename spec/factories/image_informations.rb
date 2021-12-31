d470d3b75e433580f00a56aac2909ec2f22d7b85
syumpei1997 <syumpei1997@gmail.com>
2022-01-01 00:58:49 +0900

    単体テストの実装

--- spec/factories/image_informations.rb
+++ spec/factories/image_informations.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :image_information do
-    
+    image_name           {'テスト'}
+    image_explanation    {'テスト'}
+    category_id          {'2'}
+    status_id            {'2'}
+    delivery_id          {'2'}
+    prefecture_id        {'1'}
+    nissuu_id            {'1'}
+    price                {'300'}
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/image_information_spec.rb
+++ spec/models/image_information_spec.rb
@@ -1,5 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe ImageInformation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @image_information = FactoryBot.build(:image_information)
+end
+
+describe '商品出品' do
+  context '出品できる場合' do
+    it "imageとimage_nameとimage_explanationとcategory_idとstatus_idとdelivery_idとprefecture_idとnissuu_idとpriceが存在すれば登録できる" do
+      binding.pry
+      expect(@image_information).to be_valid
+    end
+    it "priceが半角数値のみ保存可能" do
+      expect(@image_information).to be_valid
+    end
+    it "priceが¥300~¥9,999,999の間のみ保存可能" do
+      expect(@image_information).to be_valid
+    end
+  end
+
+  context '新規作成できない場合' do
+    it "imageが空では作成できない" do
+      @image_information.image = ''
+      @image_information.valid?
+      expect(@image_information.errors.full_messages).to include("Image can't be blank")
+    end
+    it "image_nameが空では作成できない" do
+      @image_information.image_name = ''
+      @image_information.valid?
+      expect(@image_information.errors.full_messages).to include("Image name can't be blank")
+    end
+    it "image_explanationが空では作成できない" do
+      @image_information.image_explanation = ''
+      @image_information.valid?
+      expect(@image_information.errors.full_messages).to include("Image explanation can't be blank")
+    end
+    it "category_idが空では作成できない" do
+      @image_information.category_id = ''
+      @image_information.valid?
+      expect(@image_information.errors.full_messages).to include("Category can't be blank")
+    end
+    it "status_idが空では作成できない" do
+      @image_information.status_id = ''
+      @image_information.valid?
+      expect(@image_information.errors.full_messages).to include("Status can't be blank")
+    end
+    it "delivery_idが空では作成できない" do
+      @image_information.delivery_id = ''
+      @image_information.valid?
+      expect(@image_information.errors.full_messages).to include("Delivery can't be blank")
+    end
+    it "prefecture_idが空では作成できない" do
+      @image_information.prefecture_id = ''
+      @image_information.valid?
+      expect(@image_information.errors.full_messages).to include("Prefecture can't be blank")
+    end
+    it "nissuu_idが空では作成できない" do
+      @image_information.nissuu_id = ''
+      @image_information.valid?
+      expect(@image_information.errors.full_messages).to include("Nissuu can't be blank")
+    end
+    it "priceが空では作成できない" do
+      @image_information.price = ''
+      @image_information.valid?
+      expect(@image_information.errors.full_messages).to include("Price can't be blank")
+    end
+  end
+ end
 end
