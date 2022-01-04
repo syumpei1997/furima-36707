ed6e2f13d2734f6048e43e04673f2dfca8b55e4b
syumpei1997 <syumpei1997@gmail.com>
2022-01-04 19:43:19 +0900

    バリテーションの追記

--- app/models/image_information.rb
+++ app/models/image_information.rb
@@ -9,12 +9,12 @@ class ImageInformation < ApplicationRecord
   validates :image,              presence: true
   validates :image_name,         presence: true
   validates :image_explanation,  presence: true
-  validates :category_id,        numericality: { other_than: 1, message: "can't be blank"} 
-  validates :status_id,          numericality: { other_than: 1, message: "can't be blank" } 
-  validates :delivery_id,        numericality: { other_than: 1, message: "can't be blank" } 
-  validates :prefecture_id,      numericality: { other_than: 1, message: "can't be blank" }
-  validates :nissuu_id,          numericality: { other_than: 1, message: "can't be blank" }
-  validates :price,              :numericality => {:only_integer => true}
+  validates :category_id,        presence: true,numericality: { other_than: 1, message: "can't be blank"} 
+  validates :status_id,          presence: true,numericality: { other_than: 1, message: "can't be blank" } 
+  validates :delivery_id,        presence: true,numericality: { other_than: 1, message: "can't be blank" } 
+  validates :prefecture_id,      presence: true,numericality: { other_than: 1, message: "can't be blank" }
+  validates :nissuu_id,          presence: true,numericality: { other_than: 1, message: "can't be blank" }
+  validates :price,              presence: true,:numericality => {:only_integer => true}
   validates :price,              numericality: { greater_than_or_equal_to: 300,less_than_or_equal_to: 9_999_999 }
   validates :price,              format: { with: /\A[0-9]+\z/ , message: "can't be blank"}
  
