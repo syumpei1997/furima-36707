5f7cebc26c8496a05ff0e932cfe0d2e9df0b7ba1
syumpei1997 <syumpei1997@gmail.com>
2022-01-05 20:53:05 +0900

    繰り返し処理

--- app/controllers/image_informations_controller.rb
+++ app/controllers/image_informations_controller.rb
@@ -1,7 +1,7 @@
 class ImageInformationsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
   def index
-    # @image_informations = ImageInformation.includes(:user)
+     @image_informations = ImageInformation.all
   end
 
   def new
