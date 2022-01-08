f43e0613a5f0de1f98fe7bfb3acddf9364a1068d
syumpei1997 <syumpei1997@gmail.com>
2022-01-08 20:24:35 +0900

    編集ページヘアクセスする

--- app/controllers/image_informations_controller.rb
+++ app/controllers/image_informations_controller.rb
@@ -22,6 +22,10 @@ class ImageInformationsController < ApplicationController
     @image_information = ImageInformation.find(params[:id])
   end
 
+  def edit
+    @image_information = ImageInformation.find(params[:id])
+  end
+
   private
 
   def image_information_params
