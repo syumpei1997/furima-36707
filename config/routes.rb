d7c400c0770f3ac805c11d362ba07f058bfd9308
syumpei1997 <syumpei1997@gmail.com>
2022-01-09 19:32:18 +0900

    商品削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    root to: 'image_informations#index'
-   resources :image_informations, only: [:new, :create, :show, :edit, :update] do
+   resources :image_informations, only: [:new, :create, :show, :edit, :update, :destroy] do
    end
 end
