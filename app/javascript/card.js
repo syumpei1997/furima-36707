f75c10ef59623aa67d626b01166277958316929a
syumpei1997 <syumpei1997@gmail.com>
2022-01-20 21:59:11 +0900

    DOMの取得

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -2,7 +2,15 @@ const pay = () => {
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("shipping_purchase[number]"),
+      exp_month: formData.get("shipping_purchase[exp_month]"),
+      exp_year: `20${formData.get("shipping_purchase[exp_year]")}`,
+    };
+    console.log(card)
   });
 };
 
