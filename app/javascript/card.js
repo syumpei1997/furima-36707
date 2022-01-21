8325234bccdd2cfc21ecdc65a6631bb07524d86e
syumpei1997 <syumpei1997@gmail.com>
2022-01-21 15:54:01 +0900

    トークンをサーバーサイドへ送る

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -15,8 +15,15 @@ const pay = () => {
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
-        console.log(token)
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
+      document.getElementById("shipping_number").removeAttribute("name");
+      document.getElementById("shipping_exp_month").removeAttribute("name");
+      document.getElementById("shipping_exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
     });
   });
 };
