dceef7e1df2539f52e5ca47e2e263a93ef37eb6e
syumpei1997 <syumpei1997@gmail.com>
2022-01-23 23:31:44 +0900

    バリテーションの追記

--- app/models/shipping_purchase.rb
+++ app/models/shipping_purchase.rb
@@ -2,16 +2,19 @@ class ShippingPurchase
   include ActiveModel::Model
   attr_accessor :postal, :prefecture_id, :municipality, :address, :building, :phone, :user_id, :image_information_id, :token
 
+  with_options presence: true do
   validate :user_id
-  validates :postal, presence: true
+  validate :image_information_id
+  validates :postal
   validates :postal, format: { with: /\A\d{3}[-]\d{4}\z/ }
-  validates :prefecture_id, presence: true
-  validates :prefecture_id,      presence: true,numericality: { other_than: 1, message: "can't be blank" }
-  validates :municipality, presence: true
-  validates :address, presence: true
-  validates :phone, presence: true
+  validates :prefecture_id
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :municipality
+  validates :address
+  validates :phone
   validates :phone, format: { with: /\A\d{10,11}\z/ }
-  validates :token, presence: true
+  validates :token
+  end
 
     def save
       purchase = Purchase.create(image_information_id: image_information_id, user_id: user_id)
