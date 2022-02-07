44022055b4560094a4e179ef479a716a4dceb64f
syumpei1997 <syumpei1997@gmail.com>
2022-02-07 15:31:36 +0900

    コミット

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,5 +14,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :name, :furigana_last_name, :furigana_name, :birthday])
   end
-  
+
 end
